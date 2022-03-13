0937b5628ce1adae3ac2f3cb7c4cbfe4456935f1
Sai Yamanoor <54855965+sai-ydev@users.noreply.github.com>
2022-03-13 14:44:38 -0400

    cleanup of main code

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,24 @@
 #include <Arduino.h>
 #include "TaskScheduler.h"
 
-void setup() {
+void t1Callback();
+
+Task t1(1000, TASK_FOREVER, &t1Callback);
+
+Scheduler scheduler;
+
+void setup()
+{
   // put your setup code here, to run once:
   Serial.begin(1000000);
+
+  scheduler.init();
+
+  // enable watchdog
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-  Serial.println("Hello World");
-  
+  scheduler.execute();
 }
