1c2d66cdb195cd0c5d9246e1ea7d3d7d22cab3fe
Sai Yamanoor <54855965+sai-ydev@users.noreply.github.com>
2022-03-13 14:22:56 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Serial_spec
+ 
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+#include "TaskScheduler.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(1000000);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Hello World");
+  
+}
